9b5e1bd6f0bb5daa2c221615b16d8d8be20d5fa3
firebolt195 <126103955+firebolt195@users.noreply.github.com>
2023-03-08 12:24:28 -0500

    Update randomImage.js

--- randomImage.js
+++ randomImage.js
@@ -2,6 +2,6 @@ window.onload = getPicture;
 var memes = new Array("memes/404image.png","memes/weird.jpg","memes/hr.png");
 
 function getPicture() {
-    var ran = Math.floor(Math.random() * 3);
+    var ran = Math.floor((Math.random() * memes.length)+1);
     document.getElementById("myPicture").src = memes[ran];
 }
